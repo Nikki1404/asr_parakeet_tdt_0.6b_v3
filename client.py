e0b73e8e16a640ed47a2c649ef1a644ecde0761b
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-03-30 17:06:35 +0530

    Update client.py

--- client.py
+++ client.py
@@ -26,3 +26,8 @@ INFO:     Waiting for application startup.
 2026-03-30 11:30:53,434 | WARNING | nv_one_logger.api.config | OneLogger: Setting error_handling_strategy to DISABLE_QUIETLY_AND_REPORT_METRIC_ERROR for rank (rank=0) with OneLogger disabled. To override: explicitly set error_handling_strategy parameter.
 2026-03-30 11:30:53,444 | INFO | nv_one_logger.exporter.export_config_manager | Final configuration contains 0 exporter(s)
 2026-03-30 11:30:53,444 | WARNING | nv_one_logger.training_telemetry.api.training_telemetry_provider | No exporters were provided. This means that no telemetry data will be collected.
+
+                                                                                                                                                    
+                                                                                                                                                    python3.11 -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
+python3.11 -c "import nemo.collections.asr as nemo_asr; print('nemo import ok')"
+python3.11 -c "import nemo.collections.asr as nemo_asr; m=nemo_asr.models.ASRModel.from_pretrained(model_name='nvidia/parakeet-tdt-0.6b-v3'); print('model load ok')"
