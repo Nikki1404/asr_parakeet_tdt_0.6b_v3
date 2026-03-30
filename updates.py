19a3b3c88bb496e4d63ded0ad37acde08c804372
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-03-30 16:37:21 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,50 +1,25 @@
-94.06 Downloading werkzeug-3.1.7-py3-none-any.whl (226 kB)
-94.07 Downloading text_unidecode-1.3-py2.py3-none-any.whl (78 kB)
-94.09 Downloading wandb-0.25.1-py3-none-manylinux_2_28_x86_64.whl (25.6 MB)
-95.07    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 25.6/25.6 MB 26.4 MB/s  0:00:00
-95.08 Downloading gitpython-3.1.46-py3-none-any.whl (208 kB)
-95.10 Downloading gitdb-4.0.12-py3-none-any.whl (62 kB)
-95.11 Downloading smmap-5.0.3-py3-none-any.whl (24 kB)
-95.13 Downloading sentry_sdk-2.56.0-py2.py3-none-any.whl (451 kB)
-95.15 Downloading wcwidth-0.6.0-py3-none-any.whl (94 kB)
-95.17 Downloading whisper_normalizer-0.1.12-py3-none-any.whl (36 kB)
-95.18 Downloading indic_numtowords-1.1.0-py3-none-any.whl (71 kB)
-95.20 Downloading wrapt-2.1.2-cp311-cp311-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl (114 kB)
-95.21 Downloading xxhash-3.6.0-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl (193 kB)
-97.83 Building wheels for collected packages: antlr4-python3-runtime, sox, kaldi-python-io, wget
-97.84   Building wheel for antlr4-python3-runtime (pyproject.toml): started
-98.12   Building wheel for antlr4-python3-runtime (pyproject.toml): finished with status 'done'
-98.12   Created wheel for antlr4-python3-runtime: filename=antlr4_python3_runtime-4.9.3-py3-none-any.whl size=144590 sha256=d7361741f8bea82af9c9fa005c99bd13bab2bc548fdf93a08188816aaa884d7e
-98.12   Stored in directory: /tmp/pip-ephem-wheel-cache-ph__41x8/wheels/1a/97/32/461f837398029ad76911109f07047fde1d7b661a147c7c56d1
-98.12   Building wheel for sox (pyproject.toml): started
-98.34   Building wheel for sox (pyproject.toml): finished with status 'done'
-98.34   Created wheel for sox: filename=sox-1.5.0-py3-none-any.whl size=40128 sha256=beb693e669fe2cc05ca8a23d54bbf3a1c3ebe2edf34a6c166c2d884c2015e073
-98.34   Stored in directory: /tmp/pip-ephem-wheel-cache-ph__41x8/wheels/74/89/93/023fcdacaec4e5471e78b43992515e8500cc2505b307e2e6b7
-98.35   Building wheel for kaldi-python-io (pyproject.toml): started
-98.56   Building wheel for kaldi-python-io (pyproject.toml): finished with status 'done'
-98.56   Created wheel for kaldi-python-io: filename=kaldi_python_io-1.2.2-py3-none-any.whl size=9006 sha256=19754d4ec26f4beb96cf1092373d7a2696dc5e58b252ac01a94f0e4f776c9e88
-98.56   Stored in directory: /tmp/pip-ephem-wheel-cache-ph__41x8/wheels/f2/86/7b/eec1bb7dc63b8aab5da6317609313873e6e75f065b65f3c29c
-98.56   Building wheel for wget (pyproject.toml): started
-98.76   Building wheel for wget (pyproject.toml): finished with status 'done'
-98.76   Created wheel for wget: filename=wget-3.2-py3-none-any.whl size=9685 sha256=9013a08b4b2d1d9fefdd68d95ceb5d58f8af250985fbf492db1a94cc7328c283
-98.76   Stored in directory: /tmp/pip-ephem-wheel-cache-ph__41x8/wheels/40/b3/0f/a40dbd1c6861731779f62cc4babcb234387e11d697df70ee97
-98.76 Successfully built antlr4-python3-runtime sox kaldi-python-io wget
-99.55 Installing collected packages: wget, text-unidecode, StrEnum, sortedcontainers, pydub, pure-eval, ptyprocess, mpmath, kaldialign, braceexpand, antlr4-python3-runtime, xxhash, wrapt, websockets, wcwidth, uvloop, urllib3, typing-extensions, traitlets, tqdm, toolz, toml, threadpoolctl, tensorboard-data-server, tabulate, sympy, smmap, six, sentencepiece, safetensors, ruamel.yaml, regex, rapidfuzz, pyyaml, python-dotenv, pyparsing, pygments, pycparser, pyarrow, psutil, protobuf, propcache, platformdirs, Pillow, pexpect, parso, packaging, overrides, nvidia-nvtx-cu12, nvidia-nvjitlink-cu12, nvidia-nccl-cu12, nvidia-curand-cu12, nvidia-cufft-cu12, nvidia-cuda-runtime-cu12, nvidia-cuda-nvrtc-cu12, nvidia-cuda-cupti-cu12, nvidia-cublas-cu12, numpy, networkx, multidict, msgpack, more_itertools, marshmallow, MarkupSafe, markdown, llvmlite, kiwisolver, joblib, intervaltree, indic-numtowords, idna, httptools, hf-xet, h11, greenlet, graphviz, fsspec, frozenlist, fonttools, filelock, executing, einops, editdistance, dill, decorator, cycler, cuda-pathfinder, colorlog, cloudpickle, click, charset_normalizer, certifi, audioread, attrs, asttokens, annotated-types, annotated-doc, aiohappyeyeballs, absl-py, yarl, whisper_normalizer, werkzeug, webdataset, uvicorn, typing-inspection, typeguard, triton, stack_data, sqlalchemy, soxr, sox, sentry-sdk, scipy, sacremoses, requests, python-dateutil, pydantic-core, prompt_toolkit, omegaconf, nvidia-cusparse-cu12, nvidia-cudnn-cu12, numexpr, numba, multiprocess, ml_dtypes, matplotlib-inline, Mako, lilcom, lightning-utilities, libcst, lazy_loader, kaldi-python-io, jiwer, jinja2, jedi, ipython-pygments-lexers, httpcore, grpcio, gitdb, cytoolz, cuda-bindings, contourpy, cffi, anyio, aiosignal, watchfiles, tensorboard, starlette, soundfile, sounddevice, scikit-learn, resampy, pyloudnorm, pydantic, pooch, pandas, onnx, nvidia-cusolver-cu12, numba-cuda, matplotlib, ipython, inflect, hydra-core, huggingface_hub, httpx, gitpython, fiddle, alembic, aiohttp, wandb, torch, tokenizers, pyannote.core, optuna, nv_one_logger_core, mediapy, librosa, fastapi, transformers, torchmetrics, pyannote-database, nv_one_logger_training_telemetry, nemo_toolkit, lhotse, datasets, accelerate, pytorch-lightning, pyannote.metrics, peft, lightning, nv_one_logger_pytorch_lightning_integration
-109.4   Attempting uninstall: packaging
-109.4     Found existing installation: packaging 26.0
-109.4     Uninstalling packaging-26.0:
-109.5       Successfully uninstalled packaging-26.0
-138.8 ERROR: Could not install packages due to an OSError: [Errno 28] No space left on device
-138.8
-138.8
+ => [6/9] RUN python3.11 -m pip install --no-cache-dir -r requirements.txt                                                                                                                                                         197.1s
+ => ERROR [7/9] RUN python3.11 - <<EOF                                                                                                                                                                                               9.1s
 ------
-Dockerfile:21
+ > [7/9] RUN python3.11 - <<EOF:
+7.598 [NeMo W 2026-03-30 11:07:23 nemo_logging:405] Megatron num_microbatches_calculator not found, using Apex version.
+8.077 OneLogger: Setting error_handling_strategy to DISABLE_QUIETLY_AND_REPORT_METRIC_ERROR for rank (rank=0) with OneLogger disabled. To override: explicitly set error_handling_strategy parameter.
+8.088 No exporters were provided. This means that no telemetry data will be collected.
+9.065 Segmentation fault (core dumped)
+------
+Dockerfile:23
 --------------------
-  19 |     COPY requirements.txt .
-  20 |     RUN python3.11 -m pip install --upgrade pip setuptools wheel
-  21 | >>> RUN python3.11 -m pip install --no-cache-dir -r requirements.txt
   22 |
-  23 |     RUN python3.11 - <<EOF
+  23 | >>> RUN python3.11 - <<EOF
+  24 | >>> import nemo.collections.asr as nemo_asr
+  25 | >>> print("Downloading Parakeet model...")
+  26 | >>> _ = nemo_asr.models.ASRModel.from_pretrained(
+  27 | >>>     model_name="nvidia/parakeet-tdt-0.6b-v3",
+  28 | >>>     map_location="cpu"
+  29 | >>> )
+  30 | >>> print("Parakeet downloaded successfully.")
+  31 | >>> EOF
+  32 |
 --------------------
-ERROR: failed to build: failed to solve: process "/bin/sh -c python3.11 -m pip install --no-cache-dir -r requirements.txt" did not complete successfully: exit code: 1
+ERROR: failed to build: failed to solve: process "/bin/sh -c python3.11 - <<EOF\nimport nemo.collections.asr as nemo_asr\nprint(\"Downloading Parakeet model...\")\n_ = nemo_asr.models.ASRModel.from_pretrained(\n    model_name=\"nvidia/parakeet-tdt-0.6b-v3\",\n    map_location=\"cpu\"\n)\nprint(\"Parakeet downloaded successfully.\")\nEOF" did not complete successfully: exit code: 139
 (base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/asr_parakeet_tdt_0.6b_v3#
