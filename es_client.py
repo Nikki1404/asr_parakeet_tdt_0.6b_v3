59c895f44ba2bc77ff9838ab71ba2df50c41fc66
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-04-02 03:56:44 +0530

    Update es_client.py

--- es_client.py
+++ es_client.py
@@ -44,12 +44,23 @@ curl -X POST http://127.0.0.1:9000/v1/audio/transcriptions \
   -F "language=es-US"
 
 
+
+this i got from logs of deployed model from docker 
+INFO:uvicorn.error:connection open
+INFO:realtime.asr.inference:Updating Riva config with session config: modalities=[<Modality.TEXT: 'text'>] input_audio_format=<AudioFormat.NONE: 'none'> input_audio_transcription=InputAudioTranscriptionConfig(language='es-US', model='parakeet-0.6b-unified-ml-cs-es-US-asr-streaming-silero-vad-sortformer', prompt=None) input_audio_params=InputAudioParams(sample_rate_hz=44100, num_channels=1) recognition_config=RecognitionConfig(max_alternatives=1, enable_automatic_punctuation=False, enable_word_time_offsets=False, enable_profanity_filter=False, enable_verbatim_transcripts=False, custom_configuration='') speaker_diarization=SpeakerDiarizationConfig(enable_speaker_diarization=False, max_speaker_count=8) word_boosting=WordBoostingConfig(enable_word_boosting=False, word_boosting_list=[]) endpointing_config=EndpointingConfig(start_history=0, start_threshold=0.0, stop_history=0, stop_threshold=0.0, stop_history_eou=0, stop_threshold_eou=0.0) input_min_chunk_seconds=0.08
+INFO:realtime.asr.inference:Starting inference task for client 35e54899-6b56-4e91-a381-d430a6ef49da
+I0331 12:31:17.397459  1212 grpc_riva_asr.cc:413] ASRService.StreamingRecognize called.
+I0331 12:31:17.400738  1212 grpc_riva_asr.cc:456] Using model parakeet-0.6b-unified-ml-cs-es-US-asr-streaming-silero-vad-sortformer from Triton localhost:8001 for inference
+INFO:realtime.asr.inference:Received end-of-stream marker for client 35e54899-6b56-4e91-a381-d430a6ef49da. Total chunks sent to server: 525
+but i am getting this 
+
 (env) root@cx-asr-test:/home/re_nikitav/parakeet-asr-multilingual# python3 transcribe_test.py 
-STATUS: 404
-TEXT: {"detail":"Model not found for language es-US"}
+STATUS: 400
+TEXT: {"detail":"Bad Request, bad model: parakeet-0.6b-unified-ml-cs-es-US-asr-streaming-silero-vad-sortformer"}
 Traceback (most recent call last):
   File "/home/re_nikitav/parakeet-asr-multilingual/transcribe_test.py", line 30, in <module>
     response.raise_for_status()
   File "/home/re_nikitav/parakeet-asr-multilingual/env/lib/python3.11/site-packages/requests/models.py", line 1028, in raise_for_status
     raise HTTPError(http_error_msg, response=self)
-requests.exceptions.HTTPError: 404 Client Error: Not Found for url: http://127.0.0.1:9000/v1/audio/transcriptions
+requests.exceptions.HTTPError: 400 Client Error: Bad Request for url: http://127.0.0.1:9000/v1/audio/transcriptions
+(env) root@cx-asr-test:/home/re_nikitav/parakeet-asr-multilingual# 
