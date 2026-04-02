c965dc1d2efb68e3ac945a9e473db9f426460d5e
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-04-02 19:46:49 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -408,3 +408,9 @@ if __name__ == "__main__":
         )
     )
 
+
+nohup python3 transcribe_selected_maria_files.py \
+  --base-url http://192.168.4.62:9000 \
+  --input-folder /home/re_nikitav/audio_maria \
+  --output-folder /home/re_nikitav/parakeet_es_results \
+  > maria_selected.log 2>&1 &
