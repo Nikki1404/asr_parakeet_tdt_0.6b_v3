8acaa684b76e6a5b680a666d881961186872f0dd
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-04-08 19:56:27 +0530

    Update es_client.py

--- es_client.py
+++ es_client.py
@@ -138,3 +138,62 @@ for lang in ["es-US", "es-ES", "es", "en-US"]:
 
     except Exception as e:
         print("ERROR:", e)
+
+
+getting this 
+Converting first 30 sec to wav...
+ffmpeg version 5.1.8-0+deb12u1 Copyright (c) 2000-2025 the FFmpeg developers
+  built with gcc 12 (Debian 12.2.0-14+deb12u1)
+  configuration: --prefix=/usr --extra-version=0+deb12u1 --toolchain=hardened --libdir=/usr/lib/x86_64-linux-gnu --incdir=/usr/include/x86_64-linux-gnu --arch=amd64 --enable-gpl --disable-stripping --enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libdav1d --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libglslang --enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg --enable-libopenmpt --enable-libopus --enable-libpulse --enable-librabbitmq --enable-librist --enable-librubberband --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libsrt --enable-libssh --enable-libsvtav1 --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzimg --enable-libzmq --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opencl --enable-opengl --enable-sdl2 --disable-sndio --enable-libjxl --enable-pocketsphinx --enable-librsvg --enable-libmfx --enable-libdc1394 --enable-libdrm --enable-libiec61883 --enable-chromaprint --enable-frei0r --enable-libx264 --enable-libplacebo --enable-librav1e --enable-shared
+  libavutil      57. 28.100 / 57. 28.100
+  libavcodec     59. 37.100 / 59. 37.100
+  libavformat    59. 27.100 / 59. 27.100
+  libavdevice    59.  7.100 / 59.  7.100
+  libavfilter     8. 44.100 /  8. 44.100
+  libswscale      6.  7.100 /  6.  7.100
+  libswresample   4.  7.100 /  4.  7.100
+  libpostproc    56.  6.100 / 56.  6.100
+[mp3 @ 0x5603d9109880] Estimating duration from bitrate, this may be inaccurate
+Input #0, mp3, from '/home/re_nikitav/parakeet-asr-multilingual/audio_maria/maria1.mp3':
+  Duration: 00:15:02.24, start: 0.000000, bitrate: 128 kb/s
+  Stream #0:0: Audio: mp3, 44100 Hz, stereo, fltp, 128 kb/s
+Stream mapping:
+  Stream #0:0 -> #0:0 (mp3 (mp3float) -> pcm_s16le (native))
+Press [q] to stop, [?] for help
+Output #0, wav, to '/tmp/maria1_30s.wav':
+  Metadata:
+    ISFT            : Lavf59.27.100
+  Stream #0:0: Audio: pcm_s16le ([1][0][0][0] / 0x0001), 16000 Hz, mono, s16, 256 kb/s
+    Metadata:
+      encoder         : Lavc59.37.100 pcm_s16le
+size=     938kB time=00:00:30.00 bitrate= 256.0kbits/s speed=75.3x    
+video:0kB audio:938kB subtitle:0kB other streams:0kB global headers:0kB muxing overhead: 0.008125%
+
+TESTING LANGUAGE = es-US
+ERROR: <_InactiveRpcError of RPC that terminated with:
+        status = StatusCode.INVALID_ARGUMENT
+        details = "Error: Unavailable model requested given these parameters: language_code=es; sample_rate=16000; type=offline; "
+        debug_error_string = "UNKNOWN:Error received from peer ipv4:192.168.4.62:50051 {grpc_status:3, grpc_message:"Error: Unavailable model requested given these parameters: language_code=es; sample_rate=16000; type=offline; "}"
+>
+
+TESTING LANGUAGE = es-ES
+ERROR: <_InactiveRpcError of RPC that terminated with:
+        status = StatusCode.INVALID_ARGUMENT
+        details = "Error: Unavailable model requested given these parameters: language_code=es; sample_rate=16000; type=offline; "
+        debug_error_string = "UNKNOWN:Error received from peer ipv4:192.168.4.62:50051 {grpc_status:3, grpc_message:"Error: Unavailable model requested given these parameters: language_code=es; sample_rate=16000; type=offline; "}"
+>
+
+TESTING LANGUAGE = es
+ERROR: <_InactiveRpcError of RPC that terminated with:
+        status = StatusCode.INVALID_ARGUMENT
+        details = "Error: Unavailable model requested given these parameters: language_code=es; sample_rate=16000; type=offline; "
+        debug_error_string = "UNKNOWN:Error received from peer ipv4:192.168.4.62:50051 {grpc_message:"Error: Unavailable model requested given these parameters: language_code=es; sample_rate=16000; type=offline; ", grpc_status:3}"
+>
+
+TESTING LANGUAGE = en-US
+ERROR: <_InactiveRpcError of RPC that terminated with:
+        status = StatusCode.INVALID_ARGUMENT
+        details = "Error: Unavailable model requested given these parameters: language_code=en; sample_rate=16000; type=offline; "
+        debug_error_string = "UNKNOWN:Error received from peer ipv4:192.168.4.62:50051 {grpc_message:"Error: Unavailable model requested given these parameters: language_code=en; sample_rate=16000; type=offline; ", grpc_status:3}"
+>
+(env) root@cx-asr-test:/home/re_nikitav/parakeet_test# 
