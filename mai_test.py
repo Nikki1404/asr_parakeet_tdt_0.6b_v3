10829075a23357fca3c45ac9deb7b0ad3c2e9326
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-04-23 16:58:21 +0530

    Update mai_test.py

--- mai_test.py
+++ mai_test.py
@@ -10,7 +10,4 @@ curl --location 'https://<YourServiceRegion>.api.cognitive.microsoft.com/speecht
   }
 }'
 
-curl --location "https://eastus.api.cognitive.microsoft.com/speechtotext/transcriptions:transcribe?api-version=2025-10-15" ^
---header "Ocp-Apim-Subscription-Key: YOUR_REAL_KEY" ^
---form "audio=@C:/Users/YourName/Desktop/audio/maria1.wav" ^
---form "definition={\"locales\":[\"en-US\",\"es-MX\"],\"enhancedMode\":{\"enabled\":true,\"model\":\"mai-transcribe-1\"}}"
+curl --location 'https://eastus.api.cognitive.microsoft.com/speechtotext/transcriptions:transcribe?api-version=2025-10-15' --header 'Ocp-Apim-Subscription-Key: YOUR_KEY_HERE' --form 'audio=@"C:\path\to\yourfile.wav"' --form 'definition={"locales":["en"],"enhancedMode":{"enabled":true,"model":"mai-transcribe-1"}}'
